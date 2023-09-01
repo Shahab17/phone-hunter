ebef4cfdccd27e714d31a57daaa92a2b7dba3b81
ShahabMat <cse_1812020090@lus.ac.bd>
2023-09-01 16:19:15 +0600

    dynamic searching

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,6 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}
+    `);
     const data = await res.json();
     const phone = data.data;
     displayPhones(phone);
@@ -11,12 +12,15 @@ const displayPhones = phone => {
 
     //1. get container div
     const phonesContainer = document.getElementById('phones-container')
+    // clear kore dibe age jeta search deya hoiche 
+    phonesContainer.textContent = '';
+
     phone.forEach(phones => {
         console.log(phones)
 
         //2. create a div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = ` card card-compact p-5 bg-red-100 shadow-xl `;
+        phoneCard.classList = ` card card-compact p-5 bg-red-50 shadow-xl `;
         // 3. set innerHTML 
         phoneCard.innerHTML = ` 
         <figure><img src="${phones.image}" /></figure>
@@ -38,8 +42,12 @@ const displayPhones = phone => {
 
 // handler buttons
 const handleSearch = () => {
-  console.log('search button')
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  // searchField.innerText = ' ';
+  console.log(searchText)
+  loadPhone(searchText);
 }
 
 
-loadPhone();
+// loadPhone();
