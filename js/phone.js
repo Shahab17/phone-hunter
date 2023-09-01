1bce5932ff336c1ddda73bc8975559109689ce82
ShahabMat <cse_1812020090@lus.ac.bd>
2023-09-01 15:42:58 +0600

    completed dynamic image set

--- js/phone.js
+++ js/phone.js
@@ -1,8 +1,45 @@
 const loadPhone = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
-    const phones = data.data;
-    console.log(phones)
+    const phone = data.data;
+    displayPhones(phone);
+    // console.log(phones)
 }
+
+const displayPhones = phone => {
+    // console.log(phones)
+
+    //1. get container div
+    const phonesContainer = document.getElementById('phones-container')
+    phone.forEach(phones => {
+        console.log(phones)
+
+        //2. create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = ` card card-compact p-5 bg-red-100 shadow-xl `;
+        // 3. set innerHTML 
+        phoneCard.innerHTML = ` 
+        <figure><img src="${phones.image}" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phones.phone_name} </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div> 
+        
+        `;
+        // 4. appendChild 
+        phonesContainer.appendChild(phoneCard);
+
+    })
+}
+
+
+// handler buttons
+const handleSearch = () => {
+  console.log('search button')
+}
+
 
 loadPhone();
